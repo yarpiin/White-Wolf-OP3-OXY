aa31e96fc725182a8612e2a1078c7c7cdc162e72
YaroST12 <yaro330@gmail.com>
2018-03-17 14:26:13 +0300

    Revert "drivers: cpufreq_interactive: use state_notifier"
    This reverts commit 45d718eca8df2ca84ad3a9c11b4e702951457ffa.

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -31,7 +31,7 @@
 #include <linux/workqueue.h>
 #include <linux/kthread.h>
 #include <linux/slab.h>
-#include <linux/state_notifier.h>
+#include <linux/display_state.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/cpufreq_interactive.h>
@@ -761,7 +761,7 @@ static int cpufreq_interactive_speedchange_task(void *data)
 	unsigned long flags;
 	struct cpufreq_interactive_policyinfo *ppol;
 	struct cpufreq_interactive_tunables *tunables;
-	bool display_on = !state_suspended;
+	bool display_on = is_display_on();
 
 	while (1) {
 		set_current_state(TASK_INTERRUPTIBLE);
