9e8d9112918778c08049908776f2f9cb8490f4aa
Slawek <yarpiin@gmail.com>
2018-06-05 17:44:17 +0100

    add yarpiin build stuff

---
+++ yarpiin-build.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
+KERNEL="Image.gz-dtb"
+DTBIMAGE="dtb"
+DEFCONFIG="yarpiin_defconfig"
+KERNEL_DIR="/home/yarpiin/Android/OP3/White-Wolf-OP3-OXY"
+RESOURCE_DIR="$KERNEL_DIR/.."
+LAZYFLASHER_DIR="/home/yarpiin/Android/OP3/lazyflasherOP3OXY"
+TOOLCHAIN_DIR="/home/yarpiin/Android/Toolchains"
+
+# Kernel Details
+BASE_YARPIIN_VER="WHITE.WOLF.OP3.OXY.ORE"
+VER=".016"
+YARPIIN_VER="$BASE_YARPIIN_VER$VER"
+
+# Vars
+export LOCALVERSION=-`echo $YARPIIN_VER`
+export CROSS_COMPILE="$TOOLCHAIN_DIR/aarch64-linux-android-7.x/bin/aarch64-linux-android-"
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER=yarpiin
+export KBUILD_BUILD_HOST=kernel
+
+# Paths
+REPACK_DIR="$LAZYFLASHER_DIR"
+PATCH_DIR="$LAZYFLASHER_DIR/ramdisk-patch"
+MODULES_DIR="$LAZYFLASHER_DIR/modules"
+ZIP_MOVE="/home/yarpiin/Android/OP3/Zip"
+ZIMAGE_DIR="$KERNEL_DIR/arch/arm64/boot"
+
+# Functions
+function clean_all {
+		if [ -f "$MODULES_DIR/*.ko" ]; then
+			rm `echo $MODULES_DIR"/*.ko"`
+		fi
+		cd $REPACK_DIR
+		rm -rf $KERNEL
+		rm -rf $DTBIMAGE
+		cd $KERNEL_DIR
+		echo
+		make clean && make mrproper
+}
+
+function make_kernel {
+		echo
+		make $DEFCONFIG
+		make $THREAD
+		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/zImage
+}
+
+function make_modules {
+		if [ -f "$MODULES_DIR/*.ko" ]; then
+			rm `echo $MODULES_DIR"/*.ko"`
+		fi
+		#find $MODULES_DIR/proprietary -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+		find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+}
+
+function make_zip {
+		cd $REPACK_DIR
+		zip -r9 `echo $YARPIIN_VER`.zip *
+		mv  `echo $YARPIIN_VER`.zip $ZIP_MOVE
+		cd $KERNEL_DIR
+}
+
+
+DATE_START=$(date +"%s")
+
+echo -e "${green}"
+echo "YARPIIN Kernel Creation Script:"
+echo
+
+echo "---------------"
+echo "Kernel Version:"
+echo "---------------"
+
+echo -e "${red}"; echo -e "${blink_red}"; echo "$YARPIIN_VER"; echo -e "${restore}";
+
+echo -e "${green}"
+echo "-----------------"
+echo "Making YARPIIN Kernel:"
+echo "-----------------"
+echo -e "${restore}"
+
+while read -p "Do you want to clean stuffs (y/n)? " cchoice
+do
+case "$cchoice" in
+	y|Y )
+		clean_all
+		echo
+		echo "All Cleaned now."
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+echo
+
+while read -p "Do you want to build kernel (y/n)? " dchoice
+do
+case "$dchoice" in
+	y|Y)
+		make_kernel
+		make_modules
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+echo
+
+while read -p "Do you want to ZIP kernel (y/n)? " dchoice
+do
+case "$dchoice" in
+	y|Y)
+		make_zip
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
+
