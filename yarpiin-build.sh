0ea3bc0b334ccdaa2bb52130df24fc023d9ffa9a
Slawek <yarpiin@gmail.com>
2018-08-22 20:55:55 +0100

    version 27

--- yarpiin-build.sh
+++ yarpiin-build.sh
@@ -20,7 +20,7 @@ TOOLCHAIN_DIR="/home/yarpiin/Android/Toolchains"
 
 # Kernel Details
 BASE_YARPIIN_VER="WHITE.WOLF.OP3.OXY.ORE"
-VER=".026"
+VER=".027"
 YARPIIN_VER="$BASE_YARPIIN_VER$VER"
 
 # Vars
